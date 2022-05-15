18e5dbffc2d35df39a3df52bf9134d9c6c93a2c8
Viktor Jensen-Torp <v.jensentorp@gmail.com>
2022-05-15 16:24:10 +0200

    update script

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const core = require('@actions/core');
+const fetch = require('node-fetch');
 
 const main = async () => {
   try {
-    // IF THIS CAN RETURN THE COMMIT SHA FROM GH- REMOVE NEED TO PASS IT TO FUNCTION!!
-    console.log('DOES THIS INCLUDE THE GITHUB_SHA?', process.env);
+    console.log('DOES THIS INCLUDE THE GITHUB_SHA?', process.env.GITHUB_SHA);
 
     const vercel_team_id = core.getInput('vercel_team_id', {required: true});
     const vercel_access_token = core.getInput('vercel_access_token', {required: true});
-    const commit_sha = core.getInput('commit_sha', {required: true});
 
     // DEBUGGING!
     console.log({vercel_access_token, vercel_team_id});
